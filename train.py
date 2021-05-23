03a325917fd58c124bdd22d05b825b4f67211246
lkwate <kwatedassiloic@gmail.com>
2021-05-23 17:46:32 +0200

    automatically detect the device and adapt the trainer's parameter accordingly

--- train.py
+++ train.py
@@ -13,18 +13,17 @@ def train(args):
 
     early_stop_callback = EarlyStopping(monitor='val_loss', patience=args.patience, mode='min', strict=False, verbose=True)
     trainer_args = {
-        'gpus' : -1,
         'max_epochs' : args.max_epochs, 
         'val_check_interval':args.val_check_interval, 'callbacks' : [early_stop_callback]
     }
+    if torch.cuda.is_available():
+        trainer_args['gpus'] = -1
     trainer = pl.Trainer(**trainer_args)
     trainer.fit(nmt_translator, dm)
 
 
 if __name__ == '__main__':
-
    args = open('config.json').read()
    args = json.loads(args)
-   args = namedtuple("args", args.kesy())(*args.values())
-
+   args = namedtuple("args", args.keys())(*args.values())
    train(args)
